1202661ea19385b2c1018cb5e984e9c046e0e054
shashidhar <s94094838@gmail.com>
2026-01-15 13:15:35 +0000

    division

--- day-4/1.py
+++ day-4/1.py
@@ -1,5 +1,7 @@
-num1=12
-num2=23
+import sys
+
+num1=int(sys.argv[1])
+num2=int(sys.argv[2])
 print(num1+num2)
 print(num1*num2)
 print(num2-num1)
@@ -7,3 +9,4 @@ add=num1+num2
 print("the value of addtion -"+str(add))
 print("the value of multiplication -"+str(num1*num2))
 print("the value of subtraction -"+str(num2-num1))
+print("the value of division -"+str(num2/num1))
